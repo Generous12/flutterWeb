ba5e2570c678a5e457a41d5d1a121e3309f2044a
Generous12 <brauliorodriguezchavez@gmail.com>
2025-11-12 15:04:12 -0500

    ASIGNACIONES HOY

--- lib/Controlador/Areas/areasService.dart
+++ lib/Controlador/Areas/areasService.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class AreaService {
   final String baseUrl =
-      "http://192.168.18.20/proyecto_web/backend/procedimientoAlm/areas_padre_sub.php";
+      "http://192.168.8.25/proyecto_web/backend/procedimientoAlm/areas_padre_sub.php";
 
   Future<Map<String, dynamic>> crearAreaPadre({
     required String nombreArea,
